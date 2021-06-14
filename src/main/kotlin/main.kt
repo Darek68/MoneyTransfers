ff0bbcdb3b33985c272d6b0665a5572e5e960d09
Darek <darek68@list.ru>
2021-06-14 23:16:55 +0300

    Second commit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,8 +5,9 @@ fun main() {
 
     print("Введите сумму перевода (в копейках): ")
     val amount = readLine()!!.toInt()
-    val commission : Int = if (amount / 10000 * COMMISSION_RATE >= COMMISSION_MINIMAL) amount / 10000 * COMMISSION_RATE else COMMISSION_MINIMAL
+    val commission = amount / 10000 * COMMISSION_RATE
+    val finishCommission : Int = if (commission >= COMMISSION_MINIMAL) commission else COMMISSION_MINIMAL
 
-    println("Cумма коммисии за перевод $amount копеек будет $commission копеек.")
+    println("Cумма коммисии за перевод $amount копеек будет $finishCommission копеек.")
 
 }
